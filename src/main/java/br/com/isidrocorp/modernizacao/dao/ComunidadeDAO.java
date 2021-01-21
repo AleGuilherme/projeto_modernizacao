c89fd36122d5315f1b98d3bcc99269858e3ff9d7
professorisidro <isidro@professorisidro.com.br>
2021-01-21 11:58:00 -0300

    modernizacao

---
+++ src/main/java/br/com/isidrocorp/modernizacao/dao/ComunidadeDAO.java
@@ -0,0 +1,9 @@
+package br.com.isidrocorp.modernizacao.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.isidrocorp.modernizacao.model.Comunidade;
+
+public interface ComunidadeDAO extends CrudRepository<Comunidade, Integer> {
+
+}
---
+++ src/main/java/br/com/isidrocorp/modernizacao/dao/ModernizacaoDAO.java
@@ -0,0 +1,9 @@
+package br.com.isidrocorp.modernizacao.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.isidrocorp.modernizacao.model.Modernizacao;
+
+public interface ModernizacaoDAO extends CrudRepository<Modernizacao,Integer>{
+
+}
